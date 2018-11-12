6148b06ff956cb6b7e97698a9a5ef381acf19a48
zawb-zhangzhichao <wb-zhangzhichao@zhongan.io>
2018-11-12 11:57:36 +0800

    mock数据

--- service/server.js
+++ service/server.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.get('*', (req, res) => {
   console.log(req.url);
-  
-  res.send(req.url)
+  const path = req.url
+  const newPath = './mock' + path + '.json'
+  res.send(require(newPath))
 })
-
 app.listen('8008')
 console.log('http://localhost:8008');
